fe8cf449340e38cec4b274b5b0b07e852c033bf1
Aditya Pal <adityapal2000.2013@gmail.com>
2025-08-23 15:26:49 +0530

    Init Starter Template

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from uvicorn import run
 # --- Configuration ---
 # It's recommended to use environment variables for these settings
 load_dotenv()
-API_KEY = os.getenv("API_KEY", "your-secret-api-key") # Replace with a secure key management system
+API_KEY = os.getenv("SECRET_KEY", "your-secret-api-key") # Replace with a secure key management system
 CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND", "redis://redis:6379/0")
 
